fe86691fec4c3cb5bb48810e402cb7a30e6783f4
Ozan Onur TEK <ozantek@std.iyte.edu.tr>
2016-03-03 01:27:32 +0200

    print2DMatrix Methods

---
+++ src/print2DMatrix.java
@@ -0,0 +1,74 @@
+	public static void print2DMatrix(int[][] n) {
+
+		for (int i = 0; i < n.length; i++) {
+			System.out.print("{");
+			for (int j = 0; j < n[0].length; j++) {
+				System.out.print(n[i][j]);
+				if (j != n[0].length - 1)
+					System.out.print(",");
+			}
+			System.out.print("}");
+			System.out.println();
+		}
+
+	}
+
+	public static void print2DMatrix(String[][] n) {
+
+		for (int i = 0; i < n.length; i++) {
+			System.out.print("{");
+			for (int j = 0; j < n[0].length; j++) {
+				System.out.print(n[i][j]);
+				if (j != n[0].length - 1)
+					System.out.print(",");
+			}
+			System.out.print("}");
+			System.out.println();
+		}
+
+	}
+
+	public static void print2DMatrix(char[][] n) {
+
+		for (int i = 0; i < n.length; i++) {
+			System.out.print("{");
+			for (int j = 0; j < n[0].length; j++) {
+				System.out.print(n[i][j]);
+				if (j != n[0].length - 1)
+					System.out.print(",");
+			}
+			System.out.print("}");
+			System.out.println();
+		}
+
+	}
+
+	public static void print2DMatrix(float[][] n) {
+
+		for (int i = 0; i < n.length; i++) {
+			System.out.print("{");
+			for (int j = 0; j < n[0].length; j++) {
+				System.out.print(n[i][j]);
+				if (j != n[0].length - 1)
+					System.out.print(",");
+			}
+			System.out.print("}");
+			System.out.println();
+		}
+
+	}
+
+	public static void print2DMatrix(double[][] n) {
+
+		for (int i = 0; i < n.length; i++) {
+			System.out.print("{");
+			for (int j = 0; j < n[0].length; j++) {
+				System.out.print(n[i][j]);
+				if (j != n[0].length - 1)
+					System.out.print(",");
+			}
+			System.out.print("}");
+			System.out.println();
+		}
+
+	}
